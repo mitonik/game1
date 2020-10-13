db857e3826f8a648240165e4b78f79093c6e5c9f
Michał Antonik <xmitonikx@gmail.com>
2020-10-13 14:17:31 +0200

    Move logic to Game class.

--- game1/Main.cpp
+++ game1/Main.cpp
@@ -1,19 +1,54 @@
 #include <SFML/Graphics.hpp>
 
+class Game {
+public:
+  Game();
+  void run();
+
+private:
+  void processEvents();
+  void update();
+  void render();
+  sf::RenderWindow window;
+  sf::CircleShape player;
+};
+
+Game::Game()
+  : window(sf::VideoMode(1280, 720), "Game 1", sf::Style::Close)
+  , player() {
+  player.setRadius(40.f);
+  player.setPosition(100.f, 100.f);
+  player.setFillColor(sf::Color::Green);
+}
+
+void Game::run() {
+  while (window.isOpen()) {
+    processEvents();
+    update();
+    render();
+  }
+}
+
+void Game::processEvents() {
+  sf::Event event;
+  while (window.pollEvent(event)) {
+    if (event.type == sf::Event::Closed) {
+      window.close();
+    }
+  }
+}
+
+void Game::update() {
+}
+
+void Game::render() {
+  window.clear();
+  window.draw(player);
+  window.display();
+}
+
 int main() {
-
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "Game 1", sf::Style::Close);
-
-	while (window.isOpen()) {
-
-		sf::Event event;
-
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
-
-	}
-	return EXIT_SUCCESS;
+  Game game;
+  game.run();
+  return EXIT_SUCCESS;
 }
