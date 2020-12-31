dda1b4f811d1eb0fc0feb004b7fef1d6d40fc40a
Michał Antonik <xmitonikx@gmail.com>
2020-12-31 16:39:48 +0100

    Change debug console behaviour.

--- game1/Main.cpp
+++ game1/Main.cpp
@@ -1,10 +1,6 @@
 #include "Application.hpp"
-#define _WIN32_WINNT 0x0500
-#include <windows.h> 
 
 int main() {
-  HWND hWnd = GetConsoleWindow();
-  ShowWindow(hWnd, SW_HIDE);
   Application app;
   app.run();
 }
